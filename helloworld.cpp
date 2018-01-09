8283ecd5be9b4a61dc9b6514762cc13a57126de4
jygnet <jyg2011@jygnet.cn>
2018-01-10 02:33:25 +0800

    test

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << "Hello github World!" << endl;    
+    std::out << "Hello github World!" << std::endl;    
 
     return 1;
 }
